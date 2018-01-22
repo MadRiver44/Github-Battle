9376c51f74d94fd1b0ebd1eaa7839ee041f3887e
Kevin Turney <super996@optonline.net>
2018-01-22 12:55:37 -0500

    basic structure, webpack, babel config

---
+++ app/Components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return <div>"Hello"</div>;
+  }
+}
+
+export default App;
+
+// to fix syntax error views https://github.com/babel/babel-sublime
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path'); // a bunch of utilities for working with file directory in node
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'), // dirname resolves to a specific directory that we specify, here
+    // it's 'build'.
+    filename: 'index_bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html',
+    }),
+  ],
+};
