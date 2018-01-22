7137a375b2f99c8fcaf10aec0e1b8008d4c45bd8
Kevin Turney <super996@optonline.net>
2018-01-22 17:54:45 -0500

    add routes

--- app/Components/App.js
+++ app/Components/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import Popular from './Popular';
+import Nav from './Nav';
+import Home from './Home';
+import Battle from './Battle';
+import Results from './Results';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="container">
-        <Popular />
-      </div>
+      <Router>
+        <div className="container">
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/battle" component={Battle} />
+            <Route path="/battle/results" component={Results} />
+            <Route path="/popular" component={Popular} />
+            <Route
+              render={function() {
+                return <p>Not Found</p>;
+              }}
+            />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
