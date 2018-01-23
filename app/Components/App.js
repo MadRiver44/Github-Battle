953c8ddfae347403759363c65b8056d6a9a5487b
Kevin Turney <super996@optonline.net>
2018-01-23 10:24:11 -0500

    entry point comment with router

--- app/Components/App.js
+++ app/Components/App.js
@@ -6,8 +6,8 @@ import Battle from './Battle';
 import Results from './Results';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
-// the entry point for the app, components are wrapped in a Router component
-// each route renders a component
+/* the entry point for the app, components are wrapped in a Router component
+ each route renders a component */
 class App extends React.Component {
   render() {
     return (
