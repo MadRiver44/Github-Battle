919e721d73286a0ab4daebf3168a9db19407a42a
Kevin Turney <super996@optonline.net>
2018-01-22 21:21:25 -0500

    Loading component

--- app/Components/Popular.js
+++ app/Components/Popular.js
@@ -93,7 +93,7 @@ class Popular extends React.Component {
           selectedLanguage={this.state.selectedLanguage}
           onSelect={this.updateLanguage}
         />
-        {!this.state.repos ? <p>Loading </p> : <RepoGrid repos={this.state.repos} />}
+        {!this.state.repos ? <Loading /> : <RepoGrid repos={this.state.repos} />}
       </div>
     );
   }
