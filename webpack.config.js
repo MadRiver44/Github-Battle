cf61d7be374e4f3970c5c822c1b1158f420877d8
Kevin Turney <super996@optonline.net>
2018-01-23 09:56:31 -0500

    edit config for production and add optimize plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path'); // a bunch of utilities for working with file directory in node
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
+let config = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'build'), // dirname resolves to a specific directory that we specify, here
@@ -24,3 +25,16 @@ module.exports = {
     }),
   ],
 };
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+  );
+}
+
+module.exports = config;
