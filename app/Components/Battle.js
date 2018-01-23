841831e0e0731859544bde1aab5e3bc0a82876f2
Kevin Turney <super996@optonline.net>
2018-01-22 20:13:39 -0500

    refactor PlayerPreview

--- app/Components/Battle.js
+++ app/Components/Battle.js
@@ -100,11 +100,7 @@ class Battle extends Component {
             <PlayerInput id="playerOne" label="Player One" onSubmit={this.handleSubmit} />
           )}
           {playerOneImage !== null && (
-            <PlayerPreview
-              avatar={playerOneImage}
-              username={playerOneName}
-              onReset={this.handleReset}
-              id="playerOne">
+            <PlayerPreview avatar={playerOneImage} username={playerOneName}>
               {' '}
               <button className="reset" onClick={this.handleReset.bind(null, 'playerOne')}>
                 Reset
@@ -112,11 +108,7 @@ class Battle extends Component {
             </PlayerPreview>
           )}
           {playerTwoImage !== null && (
-            <PlayerPreview
-              avatar={playerTwoImage}
-              username={playerTwoName}
-              onReset={this.handleReset}
-              id="playerTwo">
+            <PlayerPreview avatar={playerTwoImage} username={playerTwoName}>
               {' '}
               <button className="reset" onClick={this.handleReset.bind(null, 'playerTwo')}>
                 Reset
