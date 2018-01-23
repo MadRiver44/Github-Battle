52d1cbf337aba881d43f01a485c122e2a328a9a6
Kevin Turney <super996@optonline.net>
2018-01-22 19:27:04 -0500

    api functions

--- app/utils/api.js
+++ app/utils/api.js
@@ -2,10 +2,67 @@
 
 import axios from 'axios';
 
+const id = 'YOUR CLIENT ID';
+const sec = 'YOUR SECRET ID';
+const params = '?clent_id=' + id + '&client_secret=' + sec;
+
+// formatting data before we pass it on
+const getProfile = username => {
+  return axios.get('https://api.github.com/users/' + username + params).then(user => {
+    return user.data;
+  });
+};
+
+const getRepos = username => {
+  return axios.get('http://api.github.com/users/' + username + '/repos' + params + '&per_page=100');
+};
+
+const getStarCount = repos => {
+  return repos.data.reduce((count, repo) => {
+    return count + repo.stargazers_count;
+  }, 0);
+};
+
+const calculateScore = (profile, repos) => {
+  const followers = profile.followers;
+  const totalStars = getStarCount(repos);
+  return followers * 3 + totalStars;
+};
+
+const handleError = error => {
+  console.warn(error);
+  return null;
+};
+
+// composition of functions
+const getUserData = player => {
+  return axios.all([getProfile(player), getRepos(player)]).then(data => {
+    const profile = data[0];
+    const repos = data[1];
+
+    return {
+      profile: profile,
+      score: calculateScore(profile, repos),
+    };
+  });
+};
+
+const sortPlayers = players => {
+  return players.sort((a, b) => {
+    return b.score - a.score;
+  });
+};
+
 module.exports = {
+  battle: players => {
+    return axios
+      .all(players.map(getUserData))
+      .then(sortPlayers)
+      .catch(handleError);
+  },
+
   fetchPopularRepos: language => {
-    // encodeURI transforms hunman readable text into a utf-8 escaped string
-    let encodedURI = window.encodeURI(
+    var encodedURI = window.encodeURI(
       `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=star&order=desc&type=Repositories`,
     );
 
